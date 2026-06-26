f43533c5c1fa7b6fa574d37b6078d9e955998b27
esraaghazal <120502146+esraaghazal@users.noreply.github.com>
2026-06-26 19:53:16 +0300

    Configure firewall settings for Ubuntu
    Enabled firewalld and opened necessary ports for services.

--- scripted/allUbuntu.sh
+++ scripted/allUbuntu.sh
@@ -5,7 +5,19 @@
 swapoff -a
 
 # Disable Firewall
-systemctl disable --now ufw
+#systemctl disable --now ufw
+
+#open ports
+
+systemctl enable --now firewalld
+
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10257/tcp
+firewall-cmd --permanent --add-port=10259/tcp
+
+firewall-cmd --reload
 
 # Enable Bridge Networking
 modprobe br_netfilter
