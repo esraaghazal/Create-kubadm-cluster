95393d439d9dd76add98ad53b86d844cf02fdb15
Esraa <ghazalseraa69@gmail.com>
2026-06-26 17:00:46 +0300

    scripts file

---
+++ scripted/allAL.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#amazon linux 2023 script
+
+# to stop the script if there are errors
+set -e
+
+#Disable swap 
+swapoff -a
+
+#Disable selinux
+setenforce 0 2>/dev/null || true
+
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+systemctl disable --now firewalld 2>/dev/null || true
+
+
+#Enable Bridge Networking
+modprobe br_netfilter || true
+echo br_netfilter > /etc/modules-load.d/br_netfilter.conf
+
+
+cat <<EOF >/etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+
+#Setup Kubernetes Repo
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+EOF
+
+#Install Kubernetes & containerd
+dnf install -y kubelet kubeadm kubectl
+
+dnf install -y containerd
+
+systemctl enable --now kubelet
+systemctl enable --now containerd
+
+mkdir -p /etc/containerd
+
+containerd config default > /etc/containerd/config.toml
+
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+
+systemctl enable --now containerd
+
+systemctl restart containerd
+
+systemctl enable --now kubelet
+
+
+
---
+++ scripted/allUbuntu.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# ubuntu
+
+# Disable Swap
+swapoff -a
+
+# Disable Firewall
+systemctl disable --now ufw
+
+# Enable Bridge Networking
+modprobe br_netfilter
+echo br_netfilter | tee /etc/modules-load.d/br_netfilter.conf
+
+cat <<EOF >/etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl --system
+
+
+# Install Required Packages
+apt update
+apt install -y apt-transport-https ca-certificates curl gpg
+
+# Add Kubernetes Repository
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key \
+| gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' \
+| tee /etc/apt/sources.list.d/kubernetes.list
+
+
+apt update
+
+# Install Kubernetes
+apt install -y kubelet kubeadm kubectl
+
+apt-mark hold kubelet kubeadm kubectl
+
+# Install Containerd
+apt install -y containerd
+
+mkdir -p /etc/containerd
+
+containerd config default > /etc/containerd/config.toml
+
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+
+systemctl enable --now containerd
+
+systemctl restart containerd
+
+systemctl enable --now kubelet
+
